d4e523f11d60d72822e8cfeff51bb0f25401c948
simtimj <juliussimtim@gmail.com>
2019-05-09 19:41:23 -0700

    Initial commit.

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
